c24c6803bbffe49e3cdffa9be4c021dc1fbef9fe
watasuke102 <watasuke102@gmail.com>
2020-06-28 19:51:33 +0900

    [update] : theme select UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +14,9 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_exit_button_clicked()
+{
+    int ans = QMessageBox::question(this, tr("Warning"), tr("Changes will be discarded.\nAre you sure to exit?"));
+    if(ans == QMessageBox::Yes) exit(0);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_exit_button_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
