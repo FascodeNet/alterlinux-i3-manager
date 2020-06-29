299232d0fc356b8a6875bf04946c8d80b555ca56
watasuke102 <watasuke102@gmail.com>
2020-06-29 16:38:21 +0900

    [update] : setting init

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
 #include <QMessageBox>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //デフォルト設定
+    //テーマのデフォルト設定
+    ui->select_none->setChecked(true);
+    ui->theme_dark ->setChecked(true);
+    ui->simple     ->setChecked(true);
+    ui->bar_top    ->setChecked(true);
+    //モジュールのデフォルト設定
     //画像読み込み
     ui->none    ->setPixmap(QPixmap("./pic/polybar-none.png"    ));
     ui->round   ->setPixmap(QPixmap("./pic/polybar-round.png"   ));
--- mainwindow.h
+++ mainwindow.h
@@ -18,7 +18,11 @@ public:
 private slots:
     void on_exit_button_clicked();
 
+    void on_apply_button_clicked();
+
 private:
     Ui::MainWindow *ui;
+    void updateThemes();
+    void updateModules();
 };
 #endif // MAINWINDOW_H
