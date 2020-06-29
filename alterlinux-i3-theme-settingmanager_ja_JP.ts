8b827c5a3f9825b5b33cea76972eaf26677849bb
watasuke102 <watasuke102@gmail.com>
2020-06-29 20:55:01 +0900

    [update] : 日本語対応

--- alterlinux-i3-theme-settingmanager_ja_JP.ts
+++ alterlinux-i3-theme-settingmanager_ja_JP.ts
@@ -1,3 +1,184 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="alterlinux-polybar-theme-settingmanager_ja_JP"></TS>
+<TS version="2.1" language="ja_JP" sourcelanguage="ja_JP">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <source>polybar-config</source>
+        <translation>Polybar設定</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>Polybar Config</source>
+        <translation>Polybar設定</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <source>Theme</source>
+        <translation>テーマ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="88"/>
+        <source>Opaque bar</source>
+        <translation>バーを透明にする</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>Bottom</source>
+        <translation>下にバーを配置</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>Top</source>
+        <translation>上にバーを配置</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="115"/>
+        <source>Round off the corners</source>
+        <translation>両端の角を丸める</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="122"/>
+        <source>Light theme</source>
+        <translation>ライトテーマ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="132"/>
+        <source>Dark theme</source>
+        <translation>ダークテーマ</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="142"/>
+        <source>Colorful</source>
+        <translation>カラフル アイコン</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="149"/>
+        <source>Simple</source>
+        <translation>シンプル アイコン</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="171"/>
+        <source>none</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="178"/>
+        <source>sharprev</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="185"/>
+        <source>round</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="192"/>
+        <source>sharp</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="229"/>
+        <source>Module</source>
+        <translation>モジュール</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="246"/>
+        <source>Check the modules you want to hide</source>
+        <oldsource>Check the modules you want to hide.</oldsource>
+        <translation>非表示にしたいモジュールのチェックを外してください</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="267"/>
+        <source>Clock</source>
+        <translation>時計</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="279"/>
+        <source>i3 status</source>
+        <translation>i3 ステータス</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="291"/>
+        <source>Memory used</source>
+        <translation>メモリ使用量</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="303"/>
+        <source>Battery</source>
+        <translation>バッテリー残量</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="315"/>
+        <source>network status</source>
+        <translation>ネットワーク</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="327"/>
+        <source>CPU used</source>
+        <translation>CPU使用率</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="339"/>
+        <source>Power button</source>
+        <translation>電源</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="360"/>
+        <source>Exit</source>
+        <translation>終了</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="367"/>
+        <source>Apply</source>
+        <translation>適用</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="193"/>
+        <location filename="mainwindow.cpp" line="216"/>
+        <location filename="mainwindow.cpp" line="267"/>
+        <location filename="mainwindow.cpp" line="288"/>
+        <source>error</source>
+        <translation>エラー</translation>
+    </message>
+    <message>
+        <source>File cannnot open:</source>
+        <translation type="obsolete">ファイルを開けません: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="193"/>
+        <location filename="mainwindow.cpp" line="216"/>
+        <location filename="mainwindow.cpp" line="267"/>
+        <location filename="mainwindow.cpp" line="288"/>
+        <source>File cannnot open: </source>
+        <translation>ファイルを開けません: </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="221"/>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>finish</source>
+        <translation>完了</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="221"/>
+        <location filename="mainwindow.cpp" line="293"/>
+        <source>Completed!
+The changes will be applied when i3wm is reloaded.</source>
+        <translation>設定を変更しました。
+変更はi3wmの再起動時に適用されます。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="309"/>
+        <source>Warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="309"/>
+        <source>Changes will be discarded.
+Are you sure to exit?</source>
+        <translation>変更は破棄されます。
+終了しますか？</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    QString locale( QLocale::system().name() );
+    if(locale == QString("ja_JP"))
+    {
+        translator.load("alterlinux-i3-theme-settingmanager_ja_JP");
+        a.installTranslator(&translator);
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
