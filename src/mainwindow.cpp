94aea7aa7b4955d4847d77053f505180dff91ce6
watasuke102 <watasuke102@gmail.com>
2020-12-04 09:32:11 +0900

    [update] : 基本的なレイアウトの構成

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -13,4 +13,32 @@
 
 _MainWindow::_MainWindow(QWidget* parent)
 {
+  // Layouts
+  QVBoxLayout* layout    = new QVBoxLayout();
+  QHBoxLayout* buttons   = new QHBoxLayout();
+  // Widgets
+  QWidget*     main_widget  = new QWidget();
+  QTabWidget*  tab_widget   = new QTabWidget();
+  QPushButton* button_exit  = new QPushButton(tr("Exit"));
+  QPushButton* button_reset = new QPushButton(tr("Restore to default settings"));
+  QPushButton* button_apply = new QPushButton(tr("Apply"));
+  // Add pages to TabWidget
+  QWidget *tab_looks   = new QWidget();
+  QWidget *tab_modules = new QWidget();
+  tab_widget->addTab(tab_looks,   tr("Looks"));
+  tab_widget->addTab(tab_modules, tr("Modules"));
+  // Add widgets to layout
+  buttons->addWidget(button_exit);
+  buttons->addWidget(button_reset);
+  buttons->addStretch();
+  buttons->addWidget(button_apply);
+  QLabel* header = new QLabel(tr("Settings manager for Alter Linux i3wm edition "));
+  header->setStyleSheet("font-size:20px;");
+  layout->addWidget(header);
+  layout->addWidget(tab_widget);
+  layout->addLayout(buttons);
+  // Add layout to MainWidget
+  main_widget->setLayout(layout);
+  setCentralWidget(main_widget);
+  setFixedSize(500, 450);
 }
